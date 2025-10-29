0b2c7de4880b20998220363b5baece10f0332379
vssj-codes <vssjcodes@gmail.com>
2025-10-30 01:37:37 +0530

    ch08: build a shopping cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cart.apps.CartConfig",
     "shop.apps.ShopConfig",
 ]
 
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+CART_SESSION_ID = "cart"
