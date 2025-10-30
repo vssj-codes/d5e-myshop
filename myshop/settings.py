b17823de8c7ab1290cf573f73d39148a6be97221
vssj-codes <vssjcodes@gmail.com>
2025-10-31 00:36:23 +0530

    ch08: register customer orders

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "cart.apps.CartConfig",
+    "orders.apps.OrdersConfig",
     "shop.apps.ShopConfig",
 ]
 
