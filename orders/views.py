9b9bb9cea4e209e5238cfe272212c0703f100b77
vssj-codes <vssjcodes@gmail.com>
2025-11-01 00:07:16 +0530

    ch09: pdf (unable to install weasyprint)

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,10 @@
+import weasyprint
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import Order, OrderItem
@@ -12,13 +17,13 @@ def order_create(request):
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
-        for item in cart:
-            OrderItem.objects.create(
-                order=order,
-                product=item["product"],
-                price=item["price"],
-                quantity=item["quantity"],
-            )
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
             # clear the cart
             cart.clear()
             # launch asynchronous task
@@ -29,10 +34,27 @@ def order_create(request):
             return redirect("payment:process")
     else:
         form = OrderCreateForm()
-    return render(request, "orders/order/create.html", {"cart": cart, "form": form})
+    return render(
+        request,
+        "orders/order/create.html",
+        {"cart": cart, "form": form},
+    )
 
 
 @staff_member_required
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, "admin/orders/order/detail.html", {"order": order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string("orders/order/pdf.html", {"order": order})
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f"filename=order_{order.id}.pdf"
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(finders.find("css/pdf.css"))],
+    )
+    return response
