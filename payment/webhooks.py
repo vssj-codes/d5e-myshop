c836200eba03fb39e205a734ef7b474007eb6cb1
vssj-codes <vssjcodes@gmail.com>
2025-11-03 14:04:32 +0530

    ch10: recommendation system

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -3,6 +3,10 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
+from shop.models import Product
+from shop.recommender import Recommender
+
+from .tasks import payment_completed
 
 
 @csrf_exempt
@@ -10,14 +14,15 @@ def stripe_webhook(request):
     payload = request.body
     sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
     event = None
+
     try:
         event = stripe.Webhook.construct_event(
             payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
         )
-    except ValueError as e:
+    except ValueError:
         # Invalid payload
         return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
+    except stripe.error.SignatureVerificationError:
         # Invalid signature
         return HttpResponse(status=400)
 
@@ -28,10 +33,21 @@ def stripe_webhook(request):
                 order = Order.objects.get(id=session.client_reference_id)
             except Order.DoesNotExist:
                 return HttpResponse(status=404)
+
             # mark order as paid
             order.paid = True
+
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+
+            # save items bought for product recommendations
+            product_ids = order.items.values_list("product_id")
+            products = Product.objects.filter(id__in=product_ids)
+            r = Recommender()
+            r.products_bought(products)
+
+            # launch asynchronous task
+            payment_completed.delay(order.id)
 
     return HttpResponse(status=200)
